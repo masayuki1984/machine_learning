e4af56012935a40b22487ad1cf183431d4ebff0a
Masayuki Kato <masayuki_kato1984@icloud.com>
2017-01-08 17:23:35 +0900

    finished chapter3

--- machine_learning/chapter_03/support_vector_machine.py
+++ machine_learning/chapter_03/support_vector_machine.py
@@ -1,5 +1,4 @@
-import numpy as np
-from sklearn import datasets, tree, metrics
+from sklearn import datasets, metrics, svm
 
 # 手書き数字データの読み込み
 digits = datasets.load_digits()
@@ -17,7 +16,7 @@ images = images.reshape(images.shape[0], -1)
 # 分類器の生成
 n_samples = len(flag_3_8[flag_3_8])
 train_size = int(n_samples * 3 / 5)
-classifier = tree.DecisionTreeClassifier()
+classifier = svm.SVC(C=1.0, gamma=0.001)
 classifier.fit(images[:train_size], labels[:train_size])
 
 # 分類器の性能評価
@@ -25,3 +24,7 @@ expected = labels[train_size:]
 predicted = classifier.predict(images[train_size:])
 
 print('Accuracy:\n', metrics.accuracy_score(expected, predicted))
+print('\nConfusion metrix:\n', metrics.confusion_matrix(expected, predicted))
+print('\nPrecision:\n', metrics.precision_score(expected, predicted, pos_label=3))
+print('\nRecall:\n', metrics.recall_score(expected, predicted, pos_label=3))
+print('\nF-measure:\n', metrics.f1_score(expected, predicted, pos_label=3))
